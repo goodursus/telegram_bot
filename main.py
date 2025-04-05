418c46d7f94ef7aa10e1a6f41b3c8d17b71b8de4
Ursus <goodursus@gmail.com>
2025-04-05 21:29:41 +1100

    V1.4 without load_dotenv()

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.DEBUG)
 
 # Загружаем переменные окружения
-load_dotenv()
+#load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
